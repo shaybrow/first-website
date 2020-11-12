f36d3dc4660b691ceb87edcae5798bc252c0aad1
Seamus Brown <shay_brown_13@hotmail.com>
2020-11-11 16:06:49 -0800

    dynamic javascript

---
+++ onclick.js
@@ -0,0 +1,9 @@
+'use strict'
+
+
+
+document.getElementById("onclick").addEventListener("click", myFunction);
+
+function myFunction() {
+document.getElementById("onclick").innerHTML = "BLAST OFF"
+}
---
+++ speed.js
@@ -0,0 +1,5 @@
+'user script'
+
+let speed = prompt("How fast?")
+
+document.write('<h3>' + "You're gonna go " + speed + "!!!" +'</h3>')
